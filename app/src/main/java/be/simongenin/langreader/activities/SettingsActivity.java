e7f0155117075db32a77617fd87efd141ebb0552
Simon Genin <simongenin@hotmail.fr>
2015-06-14 18:52:41 +0200

    Add of the settings (1st implementation)

--- app/src/main/java/be/simongenin/langreader/activities/SettingsActivity.java
+++ app/src/main/java/be/simongenin/langreader/activities/SettingsActivity.java
@@ -3,6 +3,9 @@ package be.simongenin.langreader.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
 
 import be.simongenin.langreader.R;
 import butterknife.ButterKnife;
@@ -13,6 +16,18 @@ public class SettingsActivity extends AppCompatActivity {
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
 
+    @InjectView(R.id.show_definition_cb)
+    CheckBox definitionCB;
+
+    @InjectView(R.id.show_translation_cb)
+    CheckBox translationCB;
+
+    @InjectView(R.id.settings_translation_bloc)
+    RelativeLayout translationBloc;
+
+    @InjectView(R.id.settings_definition_bloc)
+    RelativeLayout definitionBloc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +37,22 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        translationBloc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the checkbox as true
+                translationCB.setChecked(!translationCB.isChecked());
+            }
+        });
+
+        definitionBloc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the checkbox as true
+                definitionCB.setChecked(!definitionCB.isChecked());
+            }
+        });
 
     }
 
